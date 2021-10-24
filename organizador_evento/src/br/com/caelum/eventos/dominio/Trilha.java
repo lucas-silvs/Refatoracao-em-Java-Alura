5409237df87a6d7790952e702ffd69b52a04acd0
Lucas Santos <lucas@aluno.faculdadeimpacta.com.br>
2021-10-24 12:11:46 -0300

    finalização do modulo 2 - extract methods e inline methods

---
+++ organizador_evento/src/br/com/caelum/eventos/dominio/Trilha.java
@@ -0,0 +1,60 @@
+package br.com.caelum.eventos.dominio;
+
+
+import static br.com.caelum.eventos.dominio.Atividade.ALMOCO;
+import static br.com.caelum.eventos.dominio.Atividade.NETWORKING;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trilha {
+	
+	private final String nome;
+	private final List<Atividade> atividadesAgendadas;
+	
+	public Trilha(String nome, SessaoDaManha sessaoDaManha, SessaoDaTarde sessaoDaTarde){
+		this.nome = nome;
+		atividadesAgendadas = new ArrayList<>();
+
+		LocalTime horarioCalculado = sessaoDaManha.comecaAs();
+		for(Palestra palestra : sessaoDaManha){
+
+			agendarPalestrar(horarioCalculado, palestra);
+
+			horarioCalculado = horarioCalculado.plusMinutes(palestra.lerTempoDeDuracao().toInt());
+		}
+		agendarAlmoco();
+		
+		horarioCalculado = sessaoDaTarde.comecaAs();
+		for(Palestra palestra : sessaoDaTarde){
+
+			agendarPalestrar(horarioCalculado, palestra);
+
+			int tempoDeDuracaoEmMinutos = palestra.lerTempoDeDuracao().toInt();
+			horarioCalculado = horarioCalculado.plusMinutes(tempoDeDuracaoEmMinutos);
+		}
+		agendarNetworking();
+	}
+
+	private void agendarPalestrar(LocalTime horarioCalculado, Palestra palestra) {
+		palestra.agendarPara(horarioCalculado);
+		atividadesAgendadas.add(palestra);
+	}
+
+	private void agendarNetworking() {
+		atividadesAgendadas.add(NETWORKING);
+	}
+
+	private void agendarAlmoco(){
+		atividadesAgendadas.add(ALMOCO);
+	}
+	
+	public String nome(){
+		return "Trilha " + nome;
+	}
+
+	public List<Atividade> lerAtividadesAgendadasEmOrdem() {
+		return atividadesAgendadas;
+	}
+}
